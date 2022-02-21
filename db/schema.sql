896696e334c0370a09dcc2d06d448680944c46b8
Ricky Leung <ricxx0320@gmail.com>
2022-02-20 20:05:52 -0800

    renamed repo, updated db

--- db/schema.sql
+++ db/schema.sql
@@ -2,26 +2,30 @@ DROP DATABASE IF EXISTS business_db;
 CREATE DATABASE business_db;
 USE business_db;
 
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,0) NOT NULL,
-  department_id INTEGER NOT NULL,
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 
 
 CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
+  role_id INT NOT NULL,
   manager_id INTEGER,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
-  CONTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELECT CASCADE
+  CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELECT CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,27 @@
 INSERT INTO department(name)
-VALUES ('Sales'),
-
+VALUES 
+  ('Sales'),
+  ('Customer Support'),
+  ('Finance'),
+  ('Policy'),
+  ('Human Resource')
+  ('IT')
 ;
 
 INSERT INTO role(id, title, salary, department_id)
-VALUES(2, 'Sales Manager', 65000, 1),
+VALUES
+  ('Sales Manager', 85000, 1),
+  ('Finance Officer', 60000, 3),
+  ('Sales Representative', 45000, 1),
+  ('Policy Advisor', 55000, 4),
+  ('Hiring Manager', 70000, 5),
+  ('Staff Coordinator', 39000, 5),
+  ('Help Desk', 55000, 6),
+  ('Customer success Manager', 60000, 2)
 ;
 
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+  ('Apple', 'Tasty', 3, 1)
 
-
-
-
-INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
-VALUES()
 ;
